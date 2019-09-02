9a899aec52f0685d42f2073e53d09695cf861df9
Mansur Ul Hasan 10P <mansoor.hasan@10pearls.com>
2019-09-02 10:11:14 +0500

    Python scripts addition

---
+++ python/1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3.5
+
+import os
+import subprocess
+import shutil
+import netifaces as ni 
+from subprocess import check_output
+
+ip = check_output(['hostname', '-I'])
+print(ip)
+
+# Copy File
+newpath = shutil.copy('hosts.txt', '/tmp/')
+
+# Get interface Info
+ip = ni.ifaddresses('eth0')
+print(ip)
